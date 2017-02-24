66809ff575b3997ccb95973d76fc0d8f9d5b27d1
dxxs19 <19lion>
2017-02-24 11:30:57 +0800

    自动换行LinearLayout功能完善

--- app/src/main/java/com/wei/customviews/view/widget/CusViewGroup.java
+++ app/src/main/java/com/wei/customviews/view/widget/CusViewGroup.java
@@ -1,17 +1,25 @@
 package com.wei.customviews.view.widget;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.wei.customviews.R;
 import com.wei.utillibrary.LogUtil;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.R.attr.gravity;
+import static android.R.attr.width;
+import static com.wei.customviews.R.attr.horizontal_Space;
+import static com.wei.customviews.R.attr.isFull;
 
 /**
  * author: WEI
@@ -21,23 +29,65 @@ import java.util.List;
 public class CusViewGroup extends ViewGroup
 {
     private final String TAG = getClass().getSimpleName();
-    private List<LineFeed> mLineFeeds = new ArrayList<>();
+    private List<LineFeed> mLineFeeds = null;
+    /*
+         *对齐方式 right 0，left 1，center 2
+        */
+    private int gravity;
+    /**
+     * 水平间距,单位px
+     */
+    private float horizontal_Space;
+    /**
+     * 垂直间距,单位px
+     */
+    private float vertical_Space;
+    /**
+     * 是否自动填满
+     */
+    private boolean isFill;
 
     public CusViewGroup(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public CusViewGroup(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public CusViewGroup(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initType(context, attrs);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public CusViewGroup(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        initType(context, attrs);
+    }
+
+    // 初始化样式，得到间隔，对齐方式等参数值
+    private void initType(Context context, AttributeSet attrs)
+    {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.CusViewGroup);
+        gravity = typedArray.getInt(R.styleable.CusViewGroup_gravity, 1);
+//        String gravityStr = typedArray.getString(R.styleable.CusViewGroup_gravity);
+//        if (gravityStr.equals("left"))
+//        {
+//            gravity = 1;
+//        }
+//        else if (gravityStr.equals("right"))
+//        {
+//            gravity = 0;
+//        }
+//        else
+//        {
+//            gravity = 2;
+//        }
+        horizontal_Space = typedArray.getDimension(R.styleable.CusViewGroup_horizontal_spacing, 0);
+        vertical_Space = typedArray.getDimension(R.styleable.CusViewGroup_vertical_spacing, 0);
+        isFill = typedArray.getBoolean(R.styleable.CusViewGroup_isFill, false);
+        typedArray.recycle();
     }
 
     @Override
@@ -45,7 +95,7 @@ public class CusViewGroup extends ViewGroup
     {
         LogUtil.e(TAG, "--- onMeasure ---");
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-        widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
         int heightMode = MeasureSpec.getMode(heightMeasureSpec);
         int heightSize = MeasureSpec.getSize(heightMeasureSpec);
         int width = 0, height = 0;
@@ -63,26 +113,22 @@ public class CusViewGroup extends ViewGroup
                 for (int i = 0; i < childCount; i ++)
                 {
                     View view = getChildAt(i);
+                    if (i != 0) {
+                        width += horizontal_Space;
+                    }
                     width += view.getMeasuredWidth();
                     LogUtil.e(TAG, "getChildAt(" + i + ").getmeasuredWidth() = " + getChildAt(i).getMeasuredWidth());
                 }
                 width += getPaddingLeft() + getPaddingRight();
                 width = width > widthSize ? widthSize : width;
-
-//                if (width > widthSize)
-//                {
-//                    // 计算行数
-//                    int row = width%widthSize == 0 ? width/widthSize : (width/widthSize + 1);
-//                    for (int i = 0; i < row; i ++)
-//                    {
-//                        height += measureHeight();
-//                    }
-//                }
                 break;
 
             case MeasureSpec.UNSPECIFIED:
                 for (int i = 0; i < childCount; i ++)
                 {
+                    if (i != 0) {
+                        width += horizontal_Space;
+                    }
                     width += getChildAt(i).getMeasuredWidth();
                 }
                 width += getPaddingLeft() + getPaddingRight();
@@ -93,11 +139,16 @@ public class CusViewGroup extends ViewGroup
                 break;
         }
 
+        /**
+         * 不能够在定义属性时初始化，因为onMeasure方法会多次调用,导致
+         * mLineFeeds尺寸比实际大
+         */
+        mLineFeeds = new ArrayList<>();
         LineFeed lineFeed = new LineFeed();
         for (int i = 0; i < childCount; i ++)
         {
             View view = getChildAt(i);
-            if (lineFeed.lineWidth + view.getMeasuredWidth() > width)
+            if (lineFeed.lineWidth + view.getMeasuredWidth() + horizontal_Space> width)
             {
                 if (lineFeed.mSameLineViews.size() == 0)
                 {
@@ -130,6 +181,10 @@ public class CusViewGroup extends ViewGroup
         height = getPaddingTop() + getPaddingBottom();
         for (int i = 0; i < mLineFeeds.size(); i ++)
         {
+            if (i != 0)
+            {
+                height += vertical_Space;
+            }
             height += mLineFeeds.get(i).height; // 行x高
         }
 
@@ -154,35 +209,46 @@ public class CusViewGroup extends ViewGroup
     }
 
     @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+    protected void onLayout(boolean changed, int l, int t, int r, int b)
+    {
         LogUtil.e(TAG, "--- onLayout ---, " + l + ", " + t + ", " + r + ", " + b);
+        t = getPaddingTop(); // 此行代码不能省，需要知道子view在容器内的paddingTop，不然有可能会出现子view显示不全的情况。
         for (int i = 0; i < mLineFeeds.size(); i ++)
-        {
+        {  // 行
             LineFeed lineFeed = mLineFeeds.get(i);
+            int left = getPaddingLeft();
+            int contentWidth = getMeasuredWidth() - lineFeed.lineWidth;
+            LogUtil.e(TAG, "lineFeed.lineWidth = " + lineFeed.lineWidth + ", contentWidth = " + contentWidth);
             List<View> views = lineFeed.mSameLineViews;
             for (int j = 0; j < views.size(); j ++)
-            {
-
+            {  // 该行有多少列(子view)
+                View view = views.get(j);
+                if (isFill) {//需要充满当前行时
+                    view.layout(left, t, left + view.getMeasuredWidth() + contentWidth / lineFeed.mSameLineViews.size(), t + view.getMeasuredHeight());
+                    left += view.getMeasuredWidth() + horizontal_Space + contentWidth / lineFeed.mSameLineViews.size();
+                } else {
+                    switch (gravity) {
+                        case 0://右对齐
+                            view.layout(left + contentWidth, t, left + contentWidth + view.getMeasuredWidth(), t + view.getMeasuredHeight());
+                            break;
+                        case 2://居中对齐
+                            view.layout(left + contentWidth / 2, t, left + contentWidth / 2 + view.getMeasuredWidth(), t + view.getMeasuredHeight());
+                            break;
+                        default://左对齐
+                            view.layout(left, t, left + view.getMeasuredWidth(), t + view.getMeasuredHeight());
+                            break;
+                    }
+                    left += view.getMeasuredWidth() + horizontal_Space;
+                }
             }
+            t += lineFeed.height + vertical_Space;
         }
-//        int size = getChildCount();
-//        int width = 0, height = 0, index = 0;
-//        count = 0;
-//        for (int i = 0; i < size; i ++)
-//        {
-//            View view = getChildAt(i);
-//            width += view.getMeasuredWidth();
-//            if ((width += (getPaddingLeft() + getPaddingRight())) > widthSize)
-//            {
-//                height += getMaxHeight(mViews.subList(index, (i==index) ? (i+1) : i));
-//                setLayout(mViews.subList(index, (i==index) ? (i+1) : i), height);
-//                index = i;
-//                width = view.getMeasuredWidth();
-//            }
-//        }
     }
 
-
+    /**
+     * 计算布局所有子view可以分成几行，
+     * LineFeed即为一行的模板，mSameLineViews即为同一行中包含有几个子view
+     */
     private final class LineFeed
     {
         private List<View> mSameLineViews = new ArrayList<>();
@@ -193,62 +259,14 @@ public class CusViewGroup extends ViewGroup
 
         private void addView(View view)
         {
+            if (mSameLineViews.size() != 0)
+            {
+                lineWidth += horizontal_Space;
+            }
             height = height > view.getMeasuredHeight() ? height : view.getMeasuredHeight();
             lineWidth += view.getMeasuredWidth();
             mSameLineViews.add(view);
         }
-    }
-
-    int count = 0;
-    private void setLayout(List<View> views, int height)
-    {
-        for (int i = 0; i < views.size(); i ++ )
-        {
-            View view = views.get(i);
-            view.layout(getPaddingLeft() + view.getMeasuredWidth() * i, view.getMeasuredHeight() * count + getPaddingTop(),
-                    view.getMeasuredWidth() * (i + 1) + getPaddingLeft(), view.getMeasuredHeight() * (count + 1) + getPaddingTop());
-        }
-        count ++;
-    }
-
-    int index = 0, width = 0, widthSize = 0;
-//    private int measureHeight()
-//    {
-//        int count = mViews.size();
-//        int height = 0;
-//        for (int i = index; i < count; i++)
-//        {
-//            View view = mViews.get(i);
-//            width += view.getMeasuredWidth();
-//            if ((width += (getPaddingLeft() + getPaddingRight())) > widthSize) {
-//                height = getMaxHeight(mViews.subList(index, (i==index) ? (i+1) : i));
-//                index = i;
-//                width = view.getMeasuredWidth();
-//                break;
-//            }
-//        }
-//        return height;
-//    }
-
-    private int getMaxHeight(List<View> views)
-    {
-        List<Integer> heights = new ArrayList<>();
-        for (View view : views)
-        {
-            int height = view.getMeasuredHeight();
-            heights.add(height);
-        }
-
-        int maxHeight = heights.get(0);
-        for (int i = 0 ; i < heights.size() - 1 ; i ++)
-        {
-            int height = heights.get(i + 1);
-            if (maxHeight < height)
-            {
-                maxHeight = height;
-            }
-        }
-        return maxHeight;
     }
 
 }
